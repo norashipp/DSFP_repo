73b3892ba7040a77572c7443072c71cb73818165
Nora Shipp <norashipp@uchicago.edu>
2017-04-24 15:54:07 -0700

    added setup.py

---
+++ build/lib/my_package/code.py
@@ -0,0 +1,5 @@
+
+def do_something():
+    a = 12**2
+    b = 15
+    print(a+b)
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='my_package',
+      version='0.1dev',
+      description='DSFP session 3, day 1',
+      author='Nora Shipp',
+      author_email='norashipp@uchicago.edu',
+      packages=['my_package'],
+     )
